8d687eaf3fde05b69a1ee2e047a6c03239428136
Eli <Elibo@Elis-iMac.local>
2016-06-11 22:40:29 +0200

    first commit

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launch : MonoBehaviour {
+
+    Rigidbody2D rb;
+    [SerializeField]
+    private float xf;
+    [SerializeField]
+    private float yf;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(new Vector2(xf,yf));
+        }
+	}
+}
